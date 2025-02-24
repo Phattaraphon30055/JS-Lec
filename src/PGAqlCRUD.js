6db1da5071c0c3da41cc8440e077bd798227d7e6
Phattaraphon30055 <6406021630055@fitm.kmutnb.ac.th>
2025-02-25 01:43:53 +0700

    save

--- src/PGAqlCRUD.js
+++ src/PGAqlCRUD.js
@@ -1,12 +1,12 @@
-// const express = require('express');
-// const Sequelize = require('sequelize');
-// const app = express();
+const express = require('express');
+const Sequelize = require('sequelize');
+const app = express();
 
-// // Parse incoming requests
-// app.use(express.json());
+// Parse incoming requests
+app.use(express.json());
 
-// // Set DB URL
-// const dbUrl = 'postgres://webadmin:MAIgsf81141@node40729-noderest.proen.app.ruk-com.cloud:11478/Books';
+// Set DB URL
+const dbUrl = 'http://node75878-env-6349498.proen.app.ruk-com.cloud:11822/';
 
-// // Create a connection to the database
-// const sequelize = new Sequelize(dbUrl);
+// Create a connection to the database
+const sequelize = new Sequelize(dbUrl);
