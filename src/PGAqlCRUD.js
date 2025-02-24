565e8ac87110c039603940d799973dc90b34813d
Phattaraphon30055 <6406021630055@fitm.kmutnb.ac.th>
2025-02-25 05:00:48 +0700

    sss

--- src/PGAqlCRUD.js
+++ src/PGAqlCRUD.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.json());
 
 // Set DB URL
-const dbUrl = 'http://node75878-env-6349498.proen.app.ruk-com.cloud:11822/';
+const dbUrl = 'postgres://webadmin:MAIgsf81141@node40729-noderest.proen.app.ruk-com.cloud:11478/Books';
+
 
 // Create a connection to the database
 const sequelize = new Sequelize(dbUrl);
